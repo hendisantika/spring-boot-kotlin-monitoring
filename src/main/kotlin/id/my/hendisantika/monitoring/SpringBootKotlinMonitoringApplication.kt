9d7b34c1d0d5fa43f4d128347c2a49284e0c2df5
hendisantika <hendisantika@yahoo.co.id>
2025-08-02 09:10:25 +0700

    feat: Add GlobalExceptionHandler🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/monitoring/SpringBootKotlinMonitoringApplication.kt
+++ src/main/kotlin/id/my/hendisantika/monitoring/SpringBootKotlinMonitoringApplication.kt
@@ -2,10 +2,26 @@ package id.my.hendisantika.monitoring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @SpringBootApplication
 class SpringBootKotlinMonitoringApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringBootKotlinMonitoringApplication>(*args)
+}
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    private val logger = KotlinLogging.logger {}
+
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Internal Server Error")
+    @ExceptionHandler(RuntimeException::class)
+    fun customerNotFound(exception: RuntimeException) {
+        logger.error(exception) { "Exception" }
+    }
 }
