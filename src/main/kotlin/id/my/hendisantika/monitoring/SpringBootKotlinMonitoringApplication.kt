5ec33756c4b5f3f69b2c2696a8bba181910e5b9b
hendisantika <hendisantika@yahoo.co.id>
2025-08-02 09:05:25 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/monitoring/SpringBootKotlinMonitoringApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.monitoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinMonitoringApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinMonitoringApplication>(*args)
+}
