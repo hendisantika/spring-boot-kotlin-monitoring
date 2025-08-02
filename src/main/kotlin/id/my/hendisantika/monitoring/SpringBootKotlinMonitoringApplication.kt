2c76910ddb447c9868a7d9b59e89fa4962dc93f0
hendisantika <hendisantika@yahoo.co.id>
2025-08-02 09:15:36 +0700

    feat: Add ok API in MessageController🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/monitoring/SpringBootKotlinMonitoringApplication.kt
+++ src/main/kotlin/id/my/hendisantika/monitoring/SpringBootKotlinMonitoringApplication.kt
@@ -1,6 +1,7 @@
 package id.my.hendisantika.monitoring
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.observation.annotation.Observed
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.HttpStatus
@@ -31,5 +32,20 @@ class MessageController(val meterRegistry: MeterRegistry) {
 
     private val logger = KotlinLogging.logger {}
 
+    @Observed(
+        name = "user.name",
+        contextualName = "getting-user-name",
+        lowCardinalityKeyValues = ["userType", "userType2"]
+    )
+    @GetMapping("/ok")
+    fun otpOk(): String {
+        logger.info { "Otp ok" }
+        val randomTime = (0..2000).random()
+        try {
+            Thread.sleep(randomTime.toLong())
+        } catch (e: InterruptedException) {
+        }
+        return "ok"
+    }
 
 }
